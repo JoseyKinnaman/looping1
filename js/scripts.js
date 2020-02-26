33725fca9ab2c6d5b7184da9c13a186a03ceedc7
Student <example@example.com>
2020-02-25 20:58:37 -0800

    Add looping

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
         var foodsInput = $("input#foods").val();
         var moviesInput = $("input#movies").val();
         var sportsInput = $("input#sports").val();
+        var favorites = [foodsInput, moviesInput, sportsInput];
+        console.log(favorites);
 
+        favorites.forEach(function(favorite) {
+            alert(favorite, "is the best");
+        });
         $(".foods").text(foodsInput);
         $(".movies").text(moviesInput);
         $(".sports").text(sportsInput);
 
         $(".favorites").show();
+
 
         event.preventDefault();
     });
